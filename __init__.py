8e298844c3ecbffc086db0c9e6f8a848e8b5c436
edisonandrade <Edisonweconnect2019>
2020-09-17 12:01:02 -0500

    Finalizacion de envio de COrreo Electronico

--- __init__.py
+++ __init__.py
@@ -5,12 +5,13 @@ import sys
 list_files = subprocess.run(["/usr/bin/python3",
                              sys.argv[1]+"/SendEmailConfig/SendEmail.py",
                              sys.argv[2],   # general properties file
-                             sys.argv[3],   # cuenta a la que se va a enviar el correo Electronico
-                             sys.argv[4],   # nombre del Usuario que va a recibir el mensaje
-                             sys.argv[5],   # numero de reporte del Usuario
-                             sys.argv[6],   # fecha de reporte del Usuario
-                             sys.argv[7],   # detalle de reporte del Usuario
-                             sys.argv[8]])  # archivo de reporte del Usuario
+                             sys.argv[3],   # general properties file
+                             sys.argv[4],   # cuenta a la que se va a enviar el correo Electronico
+                             sys.argv[5],   # nombre del Usuario que va a recibir el mensaje
+                             sys.argv[6],   # numero de reporte del Usuario
+                             sys.argv[7],   # fecha de reporte del Usuario
+                             sys.argv[8],   # detalle de reporte del Usuario
+                             sys.argv[9]])  # archivo de reporte del Usuario
 
 
 
